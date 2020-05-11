194657829f63025c1507e6e238ba42afad5a5fd9
Jake Zohdi <jakez1@live.com>
2020-05-11 12:35:14 -0400

    expand tasks in card and show number of tasks in header

--- taskmanager/frontend/src/compenents/ProjectPage/ListCard.js
+++ taskmanager/frontend/src/compenents/ProjectPage/ListCard.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Input from "@material-ui/core/Input";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import { TextField, ButtonGroup } from "@material-ui/core";
+import { TextField, ButtonGroup, IconButton } from "@material-ui/core";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import CancelIcon from "@material-ui/icons/Cancel";
 import AddIcon from "@material-ui/icons/Add";
 import { makeStyles } from "@material-ui/core/styles";
 import Task from "./Task";
-
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const useStyles = makeStyles((theme) => ({
     root: {
         width: "100%",
@@ -25,7 +26,7 @@ export default function ListCard({ list }) {
     const [state, setState] = useState(list);
     const [addNew, setAddNew] = useState(false);
     const [prevname, setPrevname] = useState(list.name);
-
+    const [expand, setExpand] = useState(true);
     const classes = useStyles();
 
     const handleName = () => {
@@ -53,64 +54,108 @@ export default function ListCard({ list }) {
         const section_list = state.id;
         addTask({ name, section_list }, handleCallback);
     };
+    const expandLess = () => {
+        setExpand(false);
+    };
     return (
-        <div style={{ paddingLeft: 10, paddingRight: 10 }}>
-            <Paper className="card-list">
-                <Input
-                    onBlur={handleName}
-                    onChange={handleNameChange}
-                    spellCheck="false"
-                    style={{ color: "black" }}
-                    value={state.name}
-                    inputProps={{
-                        "aria-label": "list title",
-                        style: {
-                            textAlign: "center",
-                            width: 100,
-                        },
+        <div
+            style={{
+                transition: "all 0.4s",
+                paddingLeft: 10,
+                minWidth: 240,
+            }}
+        >
+            <Paper className={"card-list"}>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
                     }}
-                />
-                <div className="card-list-container">
-                    {state.tasks.map((task) => {
-                        return <Task key={task.id} props={task} />;
-                    })}
-                    {addNew && (
-                        <div>
-                            <TextField
-                                style={{
-                                    marginTop: 10,
-                                }}
-                                autoFocus={true}
-                                size="small"
-                                variant="outlined"
-                                label="new task name"
-                                InputProps={{
-                                    id: "new-task-name",
-                                }}
-                            />
-                            <div style={{ textAlign: "right" }}>
-                                <ButtonGroup>
-                                    <Button onClick={handleCreateTask}>
-                                        <AddCircleOutlineIcon />
-                                    </Button>
-                                    <Button onClick={handleAddTask}>
-                                        <CancelIcon />
-                                    </Button>
-                                </ButtonGroup>
-                            </div>
-                        </div>
-                    )}
-                </div>
-                {!addNew && (
-                    <div>
-                        <Button
-                            style={{ width: "100%", marginTop: 10 }}
-                            onClick={handleAddTask}
+                >
+                    <Input
+                        onBlur={handleName}
+                        onChange={handleNameChange}
+                        spellCheck="false"
+                        style={{ color: "black" }}
+                        value={state.name}
+                        inputProps={{
+                            "aria-label": "list title",
+                            style: {
+                                textAlign: "center",
+                                width: 100,
+                            },
+                        }}
+                    />
+                    <span>
+                        <span
+                            style={{
+                                padding: 10,
+                                fontSize: 20,
+                                textAlign: "center",
+                            }}
                         >
-                            <AddIcon />
-                            Add Task
-                        </Button>
-                    </div>
+                            {state.tasks.length}
+                        </span>
+                        {expand ? (
+                            <IconButton onClick={expandLess}>
+                                <ExpandLessIcon />
+                            </IconButton>
+                        ) : (
+                            <IconButton
+                                onClick={() => {
+                                    setExpand(true);
+                                }}
+                            >
+                                <ExpandMoreIcon />
+                            </IconButton>
+                        )}
+                    </span>
+                </div>
+                {expand && (
+                    <>
+                        <div className="card-list-container">
+                            {state.tasks.map((task) => {
+                                return <Task key={task.id} props={task} />;
+                            })}
+                            {addNew && (
+                                <div>
+                                    <TextField
+                                        style={{
+                                            marginTop: 10,
+                                        }}
+                                        autoFocus={true}
+                                        size="small"
+                                        variant="outlined"
+                                        label="new task name"
+                                        InputProps={{
+                                            id: "new-task-name",
+                                        }}
+                                    />
+                                    <div style={{ textAlign: "right" }}>
+                                        <ButtonGroup>
+                                            <Button onClick={handleCreateTask}>
+                                                <AddCircleOutlineIcon />
+                                            </Button>
+                                            <Button onClick={handleAddTask}>
+                                                <CancelIcon />
+                                            </Button>
+                                        </ButtonGroup>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                        {!addNew && (
+                            <div>
+                                <Button
+                                    style={{ width: "100%", marginTop: 10 }}
+                                    onClick={handleAddTask}
+                                >
+                                    <AddIcon />
+                                    Add Task
+                                </Button>
+                            </div>
+                        )}
+                    </>
                 )}
             </Paper>
         </div>
